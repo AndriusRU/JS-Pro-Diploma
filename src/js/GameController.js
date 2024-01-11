8dba7d16d531c6010a450222c14ba20297e68492
Ячный Андрей <andrey.yachniy@ifellow.ru>
2024-01-11 19:05:17 +0700

    add step 03

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,5 +1,5 @@
 import themes from './themes';
-import {generateTeam, getAllowedCoordinates} from './generators';
+import { generateTeam, getAllowedCoordinates } from './generators';
 import Team from './Team';
 import GamePlay from './GamePlay';
 
@@ -16,6 +16,8 @@ export default class GameController {
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi(themes[1]);
     this.gamePlay.redrawPositions([...this.playerTeam, ...this.opponentTeam]);
+    this.enterOnCell();
+    this.leaveOnCell();
   }
 
   onCellClick(index) {
@@ -24,9 +26,37 @@ export default class GameController {
 
   onCellEnter(index) {
     // TODO: react to mouse enter
+    const isCharacter = this.hasCharacter(index);
+    const person = this.getCharacter(index);
+    if (isCharacter) {
+      const message = `\u{1F396}${person.character.level} \u{1F5E1}${person.character.attack} \u{1F6E1}${person.character.defence} \u{1F493}${person.character.health}`;
+      this.gamePlay.showCellTooltip(message, index);
+    }
   }
 
   onCellLeave(index) {
     // TODO: react to mouse leave
+    this.gamePlay.hideCellTooltip(index);
+  }
+
+  hasCharacter(index) {
+    const allCharacters = [...this.playerTeam, ...this.opponentTeam];
+    return allCharacters.some((person) => person.position === index);
+  }
+
+  getCharacter(index) {
+    let result = this.playerTeam.find((person) => person.position === index);
+    if (!result) {
+      result = this.opponentTeam.find((person) => person.position === index);
+    }
+    return result;
+  }
+
+  enterOnCell() {
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
+  }
+
+  leaveOnCell() {
+    this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
   }
 }
